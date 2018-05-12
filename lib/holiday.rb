d6349209c4da32208612edaddadd04b4c60335d4
Barbara Washington <barbara.m.washington@gmail.com>
2018-05-12 22:41:36 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -65,10 +65,10 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holidays|
       puts "#{season.capitalize}:"
       holiday.each do |holiday_name, supplies|
-        puts "  "
+        puts " #{holiday_name.to_s.split(pattern " ").collect {|split_holiday_name| split_holiday_name.capitalize}.join(separator " ")}: "
       end
     end
-    
+
 
 
 # Original Code:
