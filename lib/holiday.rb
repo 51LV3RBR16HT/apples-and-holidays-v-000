fe393bcd17dc851e34fefd5f3c5895472b95d9c2
Barbara Washington <barbara.m.washington@gmail.com>
2018-05-12 22:32:23 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -63,6 +63,8 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |seasons, holidays|
+      puts "#{season.capitalize}:"
+
      seasons_string = seasons.to_s
      seasons_array = seasons_string.split("_")
      seasons_array.map! do |seasons|
